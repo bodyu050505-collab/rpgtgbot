70d322d71a4ae1ddc35eb2bdff21ea512dc97501
KoToPeSpes <138239129+KoToPeSpes@users.noreply.github.com>
2026-01-15 20:23:12 +0000

    2

--- app/handlers.py
+++ app/handlers.py
@@ -5,7 +5,28 @@ import sqlite3
 
 router = Router()
 
+
+def init_db():
+    connection = sqlite3.connect("players.db", timeout=20.0)
+    cursor = connection.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Players (
+            userid PRIMARY KEY NOT NULL,
+            level INTEGER NOT NULL DEFAULT 1
+        )
+    ''')
+    connection.commit()
+    connection.close()
+
+init_db()
+
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
     print(message.from_user.first_name, message.from_user.id)
-    await message.answer("sosal da")
+    await message.answer("дароу, что бы начать игру пропиши 'В приключение!'")
+
+
+@router.message(F.text == "В приключение!")
+async def create_player(message: Message):
+    print("zafiksiroval!")
